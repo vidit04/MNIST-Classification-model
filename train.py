60a89ba66a63307b6b43c5cf5aa1ad9a4a83c945
Vidit <54926937+vidit04@users.noreply.github.com>
2020-02-12 03:51:23 +0530

    Add files via upload

--- train.py
+++ train.py
@@ -6,51 +6,89 @@ import matplotlib.pyplot as plt
 
 
 #os.chdir("C:\Users\user\Desktop\MNIST")
+def Data_pre_processing():
 
-data_image_train = gzip.open('train-images-idx3-ubyte.gz','r')
-data_label_train = gzip.open('train-labels-idx1-ubyte.gz','r')
+    data_image_train = gzip.open('train-images-idx3-ubyte.gz','r')
+    data_label_train = gzip.open('train-labels-idx1-ubyte.gz','r')
          
 
-data_image_test = gzip.open('t10k-images-idx3-ubyte.gz','r')
-data_label_test = gzip.open('t10k-labels-idx1-ubyte.gz','r')
+    data_image_test = gzip.open('t10k-images-idx3-ubyte.gz','r')
+    data_label_test = gzip.open('t10k-labels-idx1-ubyte.gz','r')
 
-data_image_train.read(16)
-data_label_train.read(8)
+    data_image_train.read(16)
+    data_label_train.read(8)
 
-data_image_test.read(16)
-data_label_test.read(8)
+    data_image_test.read(16)
+    data_label_test.read(8)
 
-image_train = data_image_train.read(28*28*60000)
-label_train = data_label_train.read(60000)
+    image_train = data_image_train.read(28*28*60000)
+    label_train = data_label_train.read(60000)
 
-image_test = data_image_test.read(28*28*10000)
-label_test = data_label_test.read(10000)
+    image_test = data_image_test.read(28*28*10000)
+    label_test = data_label_test.read(10000)
 
-image_train = np.frombuffer(image_train, dtype=np.uint8)
-label_train = np.frombuffer(label_train, dtype=np.uint8)
+    image_train = np.frombuffer(image_train, dtype=np.uint8)
+    label_train = np.frombuffer(label_train, dtype=np.uint8)
 
-image_test = np.frombuffer(image_test, dtype=np.uint8)
-label_test = np.frombuffer(label_test, dtype=np.uint8)
+    image_test = np.frombuffer(image_test, dtype=np.uint8)
+    label_test = np.frombuffer(label_test, dtype=np.uint8)
 
-image_train = image_train.reshape(60000,28,28)
-image_train = image_train[59999,:,:]
-label_train = label_train.reshape(60000)
-label_train = label_train[59999]
-print(label_train)
-#cv2.imshow('window_name', image_train)
-#cv2.waitKey(0)
-plt.imshow(image_train)
-plt.show()
+    #image_train = image_train.reshape(60000,28,28)
+    #image_train = image_train[59999,:,:]
+    image_train = image_train.reshape(60000,784)
+    label_train = label_train.reshape(60000,1)
+    #label_train = label_train[59999]
+    #print(label_train)
+    #cv2.imshow('window_name', image_train)
+    #cv2.waitKey(0)
+    #plt.imshow(image_train)
+    #plt.show()
+    one_hot_label_train = np.zeros((60000,10), dtype = np.float32)
+    for i in range(60000):
+        position_train = label_train[i,:]
+        one_hot_label_train[i,position_train] = 1.0
 
-
-image_test = image_test.reshape(10000,28,28)
-image_test = image_test[9999,:,:]
-label_test = label_test.reshape(10000)
-label_test = label_test[9999]
-print(label_test)
-#cv2.imshow('window_name', image_test)
-#cv2.waitKey(0)
-plt.imshow(image_test)
-plt.show()
+    #print(one_hot_label_train[59999,:])
 
 
+    #image_test = image_test.reshape(10000,28,28)
+    #image_test = image_test[9999,:,:]
+    image_test = image_test.reshape(10000,784)
+    label_test = label_test.reshape(10000,1)
+    #label_test = label_test[9999]
+
+    #print(label_test)
+    #cv2.imshow('window_name', image_test)
+    #cv2.waitKey(0)
+    #plt.imshow(image_test)
+    #plt.show()
+
+    one_hot_label_test = np.zeros((10000,10), dtype = np.float32)
+    for i in range(10000):
+        position_test = label_test[i,:]
+        one_hot_label_test[i,position_test] = 1.0
+
+    #print(one_hot_label_test[9999,:])   
+
+    image_valid1 = image_train[58000:,:]
+    image_valid2 = image_test[:4000,:]
+    image_valid = np.concatenate((image_valid1, image_valid2), axis=0)
+    print(len(image_valid))
+    image_train = image_train[:58000,:]
+    print(len(image_train))
+    image_test = image_test[4000:,:]
+    print(len(image_test))
+
+    one_hot_label_valid1 = one_hot_label_train[58000:,:]
+    one_hot_label_valid2 = one_hot_label_test[:4000,:]
+    one_hot_label_valid = np.concatenate((one_hot_label_valid1, one_hot_label_valid2), axis=0)
+    print(len(one_hot_label_valid))
+    one_hot_label_train = one_hot_label_train[:58000,:]
+    print(len(one_hot_label_train))
+    one_hot_label_test = one_hot_label_test[4000:,:]
+    print(len(one_hot_label_test))
+
+    return image_train, one_hot_label_train, image_valid, one_hot_label_valid, image_test, one_hot_label_test
+
+
+a,b,c,d,e,f = Data_pre_processing()
